0814f2ce92dff700dc1a4f7cad187d57c8f5c783
ChengHan111 <tankhc1@163.com>
2023-02-01 23:56:40 -0500

    update

--- get_results_PruningRewind_CVK.py
+++ get_results_PruningRewind_CVK.py
@@ -12,7 +12,7 @@ pd.set_option('display.max_rows', None)
 
 # 如果路径中有引号已经括号 需要手动删除！example: （task="closest_object_distance"） --> None 
 # 两个文件夹下:output_before_pruning + output_rewind
-root = "/home/ch7858/vpt/output_copy/output_rewind/vtab-kitti_P10_VK10_SHARED_1_INIT_2_ACC_0_ONVK_0"
+root = "/home/ch7858/vpt/output_rewind/vtab-dtd_P10_VK1_SHARED_1_INIT_2_ACC_0_BS64_LB0"
 dataset_type = 'vtab_rewind' # currently support vtab, fgvc, vtab_rewind and fgvc_rewind
 
 df_list=[]
